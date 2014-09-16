4a7cbd66165a87824fa71397591284e2be300859
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2014-09-16 14:06:07 -0600

    Add basic OpenGL setup.

--- Main.hs
+++ Main.hs
@@ -27,20 +27,95 @@ module Main where
 import Graphics.UI.GLUT
 import Data.IORef
 import System.IO
-import Data.Packed.Vector
-import Data.Packed.Matrix
+import qualified Data.Packed.Vector as V
+import qualified Data.Packed.Matrix as M
 import Numeric.GSL.ODE
 import Control.Monad
+
+import Paths_lorenz(version)
+import Data.Version(showVersion)
+
+
+
+
+
+
+
+
+-- Amount to scale unit cube orthogonal projection.
+viewScale = 3
 
 
 -- The Lorenz Attractor program.
 main :: IO ()
-main = mapM_ (printStuff) a
-    where
-        a = toLists(odeSolve (lorenz' 10 28 2.6) [1,1,1] (fromList [0,0.01..1000]))
+main = do
+        initilizeGLUT
+        mainLoop
 
-printStuff :: [Double] -> IO ()
-printStuff (x:y:z:_) = putStrLn ((show x) ++ "  " ++ (show y) ++ "  " ++ (show z))
+
+
+initilizeGLUT :: IO ()
+initilizeGLUT = do
+        -- Initilize and open window.
+        (_, _) <- getArgsAndInitialize
+        initialDisplayMode $= [RGBAMode, DoubleBuffered, WithDepthBuffer]
+        _ <- createWindow windowTitle
+        -- Callbacks.
+        specialCallback $= Nothing
+        reshapeCallback $= Just reshape
+        idleCallback    $= Nothing
+        displayCallback $= display
+        -- OpenGL settings.
+        depthFunc       $= Just Lequal
+    where
+        windowTitle = "Lorenz Atractor (v" ++ showVersion version ++ ")"
+
+
+-- This function is called by GLUT to display the scene.
+display :: DisplayCallback
+display = do
+    -- Clear and reset.
+    clear [ColorBuffer, DepthBuffer]
+    loadIdentity
+    -- Draw RGB triangle.
+    renderPrimitive Triangles $ do
+        color  $ (Color3    1.0    0.0   0.0 :: Color3 GLfloat)
+        vertex $ (Vertex2   0.0    0.5       :: Vertex2 GLfloat)
+        color  $ (Color3    0.0    1.0   0.0 :: Color3 GLfloat)
+        vertex $ (Vertex2   0.5  (-0.5)      :: Vertex2 GLfloat)
+        color  $ (Color3    0.0    0.0   1.0 :: Color3 GLfloat)
+        vertex $ (Vertex2 (-0.5) (-0.5)      :: Vertex2 GLfloat)
+    -- Draw and swap buffers.
+    flush
+    swapBuffers
+    -- Check for errors.
+    printErrors
+
+
+
+
+-- This funtion is called by GLUT when the window is resized.
+reshape :: ReshapeCallback
+reshape (Size width height) = do
+        -- Set viewport as entire window.
+        viewport $= (Position 0 0, Size width height)
+        -- Orthogonal projection: scaled cube adjusted for aspect ratio.
+        matrixMode $= Projection
+        loadIdentity
+        ortho (-w2h*viewScale) (w2h*viewScale)  -- left right
+              (-1.0*viewScale) (1.0*viewScale)  -- bottom top
+              (-1.0*viewScale) (1.0*viewScale)  -- near far
+        -- Reset model view to the identity matrix.
+        matrixMode $= Modelview 0
+        loadIdentity
+    where
+        -- Width to height ratio.
+        w2h = if height > 0
+                then (fromIntegral width)/(fromIntegral height)
+                else 1
+
+
+
 
 -- Lorenz attractor function.
 -- σ -> ρ -> β -> t -> (x, y, z) -> (dxdt, dydt, dzdt)
@@ -50,6 +125,10 @@ lorenz s r b t (x, y, z) = (dxdt, dydt, dzdt)
         dxdt = s * (y - x)
         dydt = x * (r - z) - y
         dzdt = x * y - b * z
+
+
+
+
 
 -- Same as the lorenz attractor function but uses lists instead of tuples and
 -- thus can have a runtime error.
@@ -61,17 +140,17 @@ lorenz' s r b t (x:y:z:_) = [dxdt, dydt, dzdt]
 
 
 
--- -- Font to use in glutPrint.
--- font :: BitmapFont
--- font = Helvetica18
---
---
--- -- GLUT string printer.
--- glutPrint :: String -> IO ()
--- glutPrint = renderString font 
---
---
--- -- Print OpenGL errors.
--- printErrors :: IO ()
--- printErrors = get errors >>= mapM_ (hPutStrLn stderr . show)
+-- Font to use in glutPrint.
+font :: BitmapFont
+font = Helvetica18
+
+
+-- GLUT string printer.
+glutPrint :: String -> IO ()
+glutPrint = renderString font 
+
+
+-- Print OpenGL errors.
+printErrors :: IO ()
+printErrors = get errors >>= mapM_ (hPutStrLn stderr . show)
 --
