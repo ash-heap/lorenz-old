6b5408d5dec6d95a60c3fb10858290b7350cce46
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2014-09-16 18:54:56 -0600

    Add settable view.

--- Main.hs
+++ Main.hs
@@ -21,16 +21,13 @@
  --  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ------------------------------------------------------------------------------}
 
+{-# OPTIONS_GHC -O2 -Wall -Werror #-}
 
 module Main where
 
-import Data.Number.FloatToFloat
 import Graphics.UI.GLUT
 import Data.IORef
 import System.IO
-import qualified Data.Packed.Vector as V
-import qualified Data.Packed.Matrix as M
-import Numeric.GSL.ODE
 
 import Paths_lorenz(version)
 import Data.Version(showVersion)
@@ -43,26 +40,52 @@ type Vector3f = Vector3 GLfloat
 
 
 -- Helpful base vectors.
-zeroVector3f = Vector3 0.0 0.0 0.0 :: Vector3f
-xVector3f    = Vector3 1.0 0.0 0.0 :: Vector3f
-yVector3f    = Vector3 0.0 1.0 0.0 :: Vector3f
-zVector3f    = Vector3 0.0 0.0 1.0 :: Vector3f
+zeroVector3f :: Vector3f
+zeroVector3f = Vector3 0.0 0.0 0.0
+xVector3f :: Vector3f
+xVector3f = Vector3 1.0 0.0 0.0
+yVector3f :: Vector3f
+yVector3f = Vector3 0.0 1.0 0.0
+zVector3f :: Vector3f
+zVector3f = Vector3 0.0 0.0 1.0
 
 -- Helpful base vectors.
-originVertex3f = Vertex3 0.0 0.0 0.0 :: Vertex3f
-xVertex3f    = Vertex3 1.0 0.0 0.0 :: Vertex3f
-yVertex3f    = Vertex3 0.0 1.0 0.0 :: Vertex3f
-zVertex3f    = Vertex3 0.0 0.0 1.0 :: Vertex3f
+originVertex3f :: Vertex3f
+originVertex3f = Vertex3 0.0 0.0 0.0
+xVertex3f :: Vertex3f
+xVertex3f = Vertex3 1.0 0.0 0.0
+yVertex3f :: Vertex3f
+yVertex3f = Vertex3 0.0 1.0 0.0
+zVertex3f :: Vertex3f
+zVertex3f = Vertex3 0.0 0.0 1.0
 
 -- Some base colors.
-black   = Color3 0.0 0.0 0.0 :: Color3f
-white   = Color3 1.0 1.0 1.0 :: Color3f
-red     = Color3 1.0 0.0 0.0 :: Color3f
-green   = Color3 0.0 1.0 0.0 :: Color3f
-blue    = Color3 0.0 0.0 1.0 :: Color3f
-cyan    = Color3 0.0 1.0 1.0 :: Color3f
+black :: Color3f
+black = Color3 0.0 0.0 0.0 :: Color3f
+white :: Color3f
+white = Color3 1.0 1.0 1.0 :: Color3f
+red :: Color3f
+red = Color3 1.0 0.0 0.0 :: Color3f
+green :: Color3f
+green = Color3 0.0 1.0 0.0 :: Color3f
+blue :: Color3f
+blue = Color3 0.0 0.0 1.0 :: Color3f
+cyan :: Color3f
+cyan = Color3 0.0 1.0 1.0 :: Color3f
+magenta :: Color3f
 magenta = Color3 1.0 0.0 1.0 :: Color3f
-yellow  = Color3 1.0 1.0 0.0 :: Color3f
+yellow :: Color3f
+yellow = Color3 1.0 1.0 0.0 :: Color3f
+
+
+
+vertexToVector3 :: Vertex3 a -> Vector3 a
+vertexToVector3 (Vertex3 x y z) = (Vector3 x y z)
+
+
+vectorToVertex3 :: Vector3 a -> Vertex3 a
+vectorToVertex3 (Vector3 x y z) = (Vertex3 x y z)
+
 
 
 -- Contains the values related to the view.
@@ -88,8 +111,8 @@ initialView :: View
 initialView = View 
     10.0 -- scale
     (Vertex3 0.0 0.0 0.0) -- center
-    45.0 -- azimuth
-    45.0 -- elevation
+    (-20.0) -- azimuth
+    20.0 -- elevation
 
 
 
@@ -113,16 +136,16 @@ initilizeGLUT appRef = do
         -- Initilize and open window.
         (_, _) <- getArgsAndInitialize
         initialDisplayMode $= [RGBAMode, DoubleBuffered, WithDepthBuffer]
-        _ <- createWindow windowTitle
+        _ <- createWindow title
         -- Callbacks.
         specialCallback $= Nothing
-        reshapeCallback $= Just (reshape appRef)
+        reshapeCallback $= Just reshape
         idleCallback    $= Nothing
         displayCallback $= display appRef
         -- OpenGL settings.
         depthFunc       $= Just Lequal
     where
-        windowTitle = "Lorenz Atractor (v" ++ showVersion version ++ ")"
+        title = "Lorenz Atractor (v" ++ showVersion version ++ ")"
 
 
 
@@ -130,11 +153,14 @@ initilizeGLUT appRef = do
 -- This function is called by GLUT to display the scene.
 display :: IORef App -> DisplayCallback
 display appRef = do
-        -- Clear and reset.
+        app <- readIORef appRef
+        -- Clear and set the view.
         clear [ColorBuffer, DepthBuffer]
-        loadIdentity
+        applyView . appView $ app
         -- Draw axis.
-        readIORef appRef >>= drawAxes . viewScale . appView
+        preservingMatrix $ do
+            (\s -> scale s s s) . viewScale . appView $ app
+            drawAxes
         -- Draw and swap buffers.
         flush
         swapBuffers
@@ -143,21 +169,31 @@ display appRef = do
 
 
 
--- This funtion is called by GLUT when the window is resized.
-reshape :: IORef App -> ReshapeCallback
-reshape appRef (Size width height) = do
-        -- Set viewport as entire window.
-        viewport $= (Position 0 0, Size width height)
-        -- Orthogonal projection: scaled cube adjusted for aspect ratio.
-        s <- readIORef appRef >>= return . floatToFloat . viewScale . appView
+
+applyView :: View -> IO ()
+applyView (View scaleFactor center azimuth elevation) = do
+        loadIdentity
+        scale s s s
+        translate $ fmap negate (vertexToVector3 center)
+        rotate elevation xVector3f
+        rotate azimuth yVector3f
+        rotate (-90) zVector3f
+        rotate (-90) yVector3f
+    where
+        s = 1.0/scaleFactor
+
+
+
+applyProjection :: Size -> IO ()
+applyProjection (Size width height) = do
+        -- Orthogonal projection: unit cube adjusted for aspect ratio.
         matrixMode $= Projection
         loadIdentity
         if w2h >= 1
-            then ortho (-w2h*s) (w2h*s) (-1.0*s) (1.0*s) (-1.0*s) (1.0*s)
-            else ortho (-1.0*s) (1.0*s) (-s/w2h) (s/w2h) (-1.0*s) (1.0*s)
-        -- Reset model view to the identity matrix.
-        matrixMode $= Modelview 0
-        loadIdentity
+            -- Wider than taller.
+            then ortho (-w2h) (w2h) (-1.0)   (1.0)   (-1.0) (1.0)
+            -- Taller than wider.
+            else ortho (-1.0) (1.0) (-1/w2h) (1/w2h) (-1.0) (1.0)
     where
         -- Width to height ratio.
         w2h = if height > 0
@@ -165,10 +201,22 @@ reshape appRef (Size width height) = do
                 else 1
 
 
+-- This funtion is called by GLUT when the window is resized.
+reshape :: ReshapeCallback
+reshape size = do
+        -- Set viewport as entire window.
+        viewport $= (Position 0 0, size)
+        -- Apply the projection.
+        applyProjection size
+        -- Reset the modelview matrix.
+        matrixMode $= Modelview 0
+        loadIdentity
 
-drawAxes :: GLfloat -> IO ()
-drawAxes scaleFactor = preservingMatrix $ do
-        scale axisLength axisLength axisLength
+
+
+drawAxes :: IO ()
+drawAxes = preservingMatrix $ do
+        scale s s s
         -- Axes lines.
         drawVector red   xVector3f
         drawVector green yVector3f
@@ -176,7 +224,7 @@ drawAxes scaleFactor = preservingMatrix $ do
         -- Draw point at origin.
         drawPoint 5.0 white originVertex3f
     where
-        axisLength = 0.7*scaleFactor
+        s = 0.7 :: GLfloat
 
 
 
@@ -204,7 +252,7 @@ drawVector c (Vector3 x y z) =
 -- Lorenz attractor function.
 -- σ -> ρ -> β -> t -> (x, y, z) -> (dxdt, dydt, dzdt)
 lorenz :: Num a => a -> a -> a -> a -> (a, a, a) -> (a, a, a)
-lorenz s r b t (x, y, z) = (dxdt, dydt, dzdt)
+lorenz s r b _ (x, y, z) = (dxdt, dydt, dzdt)
     where
         dxdt = s * (y - x)
         dydt = x * (r - z) - y
@@ -221,6 +269,7 @@ lorenz' :: Num a => a -> a -> a -> a -> [a] -> [a]
 lorenz' s r b t (x:y:z:_) = [dxdt, dydt, dzdt]
     where 
         (dxdt, dydt, dzdt) = lorenz s r b t (x, y, z)
+lorenz' _ _ _ _ _ = error "lorenz' needs a list with exactly 3 elements"
 
 
 
